f31c24db6986b8d7a5b54df55b25a2895fd864f4
Isabel Feraudo <natyferaudo23@gmail.com>
2024-07-20 17:56:39 -0300

    class auto and Lugar

--- src/Simulation.js
+++ src/Simulation.js
@@ -71,98 +71,19 @@ class Simulation {
   }
 }
 
-class LlegadaCliente {
-  constructor(tiempoActual, nroCliente) {
-    this.tiempo = tiempoActual + -3 * Math.log(1 - Math.random())
-    this.nroCliente = nroCliente
-  }
-
-  ocurreEvento(datos) {
-    datos.clientes += 1
-    datos.colaEventos.push(new LlegadaCliente(this.tiempo, datos.clientes + 1))
-
-    if (datos.stock === 0) {
-      datos.clientesTristes += 1
-    } else if (datos.empleadosLibres > 0) {
-      datos.colaEventos.push(new FinAtencion(this.tiempo, this.nroCliente))
-      datos.empleadosLibres -= 1
-    } else {
-      datos.colaClientes.push(this.nroCliente)
-      datos.colaEventos.push(new ClienteSeRetiraPorTiempo(this.tiempo, this.nroCliente))
-    }
+class Auto {
+  constructor(nro, tamano, estado, lugar, costo) {
+    this.nro = nro
+    this.tamano = tamano // pequeño
+    this.estado = estado // estacionado, esperando pagar, pagando
+    this.lugar = lugar // { tipo: grande }
+    this.costo = costo
   }
 }
 
-class FinAtencion {
-  constructor(tiempoActual, nroCliente) {
-    this.tiempo = tiempoActual + (0.5 + Math.random())
-    this.nroCliente = nroCliente
-  }
-
-  ocurreEvento(datos) {
-    const productosPedidos = Math.floor(Math.random() * 3) + 1
-
-    if (datos.stock === 0) {
-      datos.clientesTristes += 1
-    } else {
-      datos.stock = productosPedidos > datos.stock ? 0 : datos.stock - productosPedidos
-
-      if (datos.stock === 0) {
-        datos.clientesTristes += datos.colaClientes.length
-        datos.colaClientes = []
-        datos.colaEventos = datos.colaEventos.filter(evento => !(evento instanceof EncendidoHorno45Minutos))
-        datos.colaEventos.push(new FinCoccionHorno(this.tiempo, 0))
-      }
-    }
-
-    if (datos.colaClientes.length > 0) {
-      const nroCliente = datos.colaClientes.shift()
-      datos.colaEventos.push(new FinAtencion(this.tiempo, nroCliente))
-      datos.colaEventos = datos.colaEventos.filter(
-        evento => !(evento instanceof ClienteSeRetiraPorTiempo && evento.nroCliente === nroCliente)
-      )
-    } else {
-      datos.empleadosLibres += 1
-    }
+class Lugar {
+  constructor(tamano, ocupados) {
+    this.tamano = tamano // grande, pequeño, utilitario
+    this.ocupados = ocupados // 0, 1, 2
   }
 }
-
-class FinCoccionHorno {
-  constructor(tiempoActual, stock) {
-    if (stock === 0) {
-      this.produccion = 45
-      this.tiempo = tiempoActual + 10.9
-    } else {
-      this.produccion = 30
-      this.tiempo = tiempoActual + 9.2
-    }
-  }
-
-  ocurreEvento(datos) {
-    datos.stock += this.produccion
-    datos.colaEventos.push(new EncendidoHorno45Minutos(this.tiempo))
-  }
-}
-
-class EncendidoHorno45Minutos {
-  constructor(tiempoActual) {
-    this.tiempo = tiempoActual + 45
-  }
-
-  ocurreEvento(datos) {
-    datos.colaEventos.push(new FinCoccionHorno(this.tiempo, datos.stock))
-  }
-}
-
-class ClienteSeRetiraPorTiempo {
-  constructor(tiempoActual, nroCliente) {
-    this.tiempo = tiempoActual + 5
-    this.nroCliente = nroCliente
-  }
-
-  ocurreEvento(datos) {
-    datos.colaClientes = datos.colaClientes.filter(cliente => cliente !== this.nroCliente)
-  }
-}
-
-export default Simulation
